1ef9e09adf5e190e9140223549c200189452d3c5
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2021-03-11 20:00:16 +1300

    Add yjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useMemo, useEffect } from 'react'
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import { useYDoc, useYArray } from 'zustand-yjs'
+
+const connectDoc = doc => {
+  const provider = new WebrtcProvider('testyroom', doc, {})
+  return () => console.log('dc') || provider.disconnect()
+}
+
 const App = () => {
+  const yDoc = useYDoc('myDocGuid', connectDoc)
+  const { data, push } = useYArray(yDoc.getArray('usernames'))
   return (
-    <div className="App">
-      123123
+    <div>
+      <button onClick={() => push([`username #${data.length}`])}>
+        New Username
+      </button>
+      <ul>
+        {data.map((username, index) => (
+          <li key={index}>{username}</li>
+        ))}
+      </ul>
     </div>
   )
 }
